e26a89a8b32d83e9d9ecb5c3325f454e6851aecf
Sai Krishna <krishnasai111@gmail.com>
2018-10-27 12:45:18 +0530

    file updated

--- arrays.js
+++ arrays.js
@@ -6,8 +6,8 @@ var ar=[10,20,30,40,50];
 //console.log(ar[i]);
 //sum=sum+ar[i];
 
-//ar.push(60);
-ar.pop(1);
-console.log(ar);
+	//ar.push(60);
+	ar.pop(1);
+	console.log(ar);
 }
 //console.log("Sum Result: " + sum);
